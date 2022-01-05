6b0c9a60c69bcc7018494babddbc7ddf318f3468
Kyloc21 <liuzeduo1234@gmail.com>
2022-01-05 16:24:35 +0800

    feat: [Box] [genBox] [defBox] basic

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-export { greet };
-const greet = (name: string) => `Hello ${name}!`;
+import defBox from "./define";
+import genBox from "./generate";
+import { Box, BoxProps } from "./base";
+
+export default Box;
+export { genBox, defBox };
+export type { BoxProps };
